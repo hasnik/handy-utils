0822005ce95c3cb564f7ee3522111271dc23a516
Michał Haśnik <Michal.Hasnik@sdimedia.com>
2020-10-06 15:35:28 +0200

    Improve buffers xmldocs

--- HandyUtils.Buffers/AsyncBuffer.cs
+++ HandyUtils.Buffers/AsyncBuffer.cs
@@ -3,19 +3,27 @@ using System.Buffers;
 
 namespace HandyUtils.Buffers
 {
+    /// <summary>
+    ///     Wrapper that helps with buffer management in asynchronous scenarios.
+    ///     Gives access to trimmed contiguous region of memory through Memory field.
+    /// </summary>
+    /// <typeparam name="T">Type of data to hold in buffer</typeparam>
     public readonly struct AsyncBuffer<T> : IDisposable
     {
+        /// <value>
+        /// Gives access to trimmed contiguous region of buffer memory
+        /// </value>
         public readonly Memory<T> Memory;
 
         private readonly T[] _rentedArray;
         private readonly bool _clearBufferOnReturn;
 
         /// <summary>
-        ///     Returns passed preallocated buffer if size is sufficient or creates a buffer for temporary data using ArrayPool.
+        ///     Wraps preallocated buffer if size is sufficient or creates a buffer for temporary data using ArrayPool.
         ///     Consume via using statement.
         /// </summary>
         /// <param name="preallocatedBuffer">Pass a preallocated buffer from consuming method</param>
-        /// <param name="bufferSize">Size of buffer to return</param>
+        /// <param name="bufferSize">Size of buffer needed</param>
         /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose (default: false)</param>
         public AsyncBuffer(Memory<T> preallocatedBuffer, int bufferSize, bool clearBufferOnReturn = false)
         {
@@ -36,8 +44,8 @@ namespace HandyUtils.Buffers
         /// <summary>
         ///     Creates a buffer for temporary data using ArrayPool. Consume via using statement.
         /// </summary>
-        /// <param name="bufferSize">Size of buffer to return</param>
-        /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose</param>
+        /// <param name="bufferSize">Size of buffer needed</param>
+        /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose (default: false)</param>
         public AsyncBuffer(int bufferSize, bool clearBufferOnReturn = false)
         {
             _rentedArray = ArrayPool<T>.Shared.Rent(bufferSize);
--- HandyUtils.Buffers/Buffer.cs
+++ HandyUtils.Buffers/Buffer.cs
@@ -3,19 +3,27 @@ using System.Buffers;
 
 namespace HandyUtils.Buffers
 {
+    /// <summary>
+    ///     Wrapper that helps with buffer management in synchronous scenarios.
+    ///     Gives access to trimmed contiguous region of memory through Span field.
+    /// </summary>
+    /// <typeparam name="T">Type of data to hold in buffer</typeparam>
     public readonly ref struct Buffer<T>
     {
+        /// <value>
+        /// Gives access to trimmed contiguous region of buffer memory
+        /// </value>
         public readonly Span<T> Span;
 
         private readonly T[] _rentedArray;
         private readonly bool _clearBufferOnReturn;
 
         /// <summary>
-        ///     Returns passed preallocated buffer if size is sufficient or creates a buffer for temporary data using ArrayPool.
+        ///     Wraps preallocated buffer if size is sufficient or creates a buffer for temporary data using ArrayPool.
         ///     Consume via using statement.
         /// </summary>
         /// <param name="preallocatedBuffer">Pass a preallocated buffer from consuming method (consider using stackalloc)</param>
-        /// <param name="bufferSize">Size of buffer to return</param>
+        /// <param name="bufferSize">Size of buffer needed</param>
         /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose (default: false)</param>
         public Buffer(Span<T> preallocatedBuffer, int bufferSize, bool clearBufferOnReturn = false)
         {
@@ -36,8 +44,8 @@ namespace HandyUtils.Buffers
         /// <summary>
         ///     Creates a buffer for temporary data using ArrayPool. Consume via using statement.
         /// </summary>
-        /// <param name="bufferSize">Size of buffer to return</param>
-        /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose</param>
+        /// <param name="bufferSize">Size of buffer needed</param>
+        /// <param name="clearBufferOnReturn">When true zeroes-out buffer memory on dispose (default: false)</param>
         public Buffer(int bufferSize, bool clearBufferOnReturn = false)
         {
             _rentedArray = ArrayPool<T>.Shared.Rent(bufferSize);
