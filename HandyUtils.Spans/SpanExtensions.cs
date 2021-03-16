0e05d8840d77f51b1f03186f19ea5672a745495c
Michał Haśnik <Michal.Hasnik@sdimedia.com>
2021-03-16 14:47:49 +0100

    Added Span split

---
+++ HandyUtils.Spans/SpanExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HandyUtils.Spans
+{
+    public static class SpanExtensions
+    {
+        public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span)
+            => new SpanSplitEnumerator<char>(span, ' ');
+
+        public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, char separator)
+            => new SpanSplitEnumerator<char>(span, separator);
+
+        public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, string separator)
+            => new SpanSplitEnumerator<char>(span, separator ?? string.Empty);
+    }
+}
