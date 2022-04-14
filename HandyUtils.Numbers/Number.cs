0fc037b6e91d8f38a19de021808deb2dae085c8f
Michał Haśnik <pasniken@gmail.com>
2022-04-14 16:30:08 +0200

    Simplified IsDivisibleBy4 method

--- HandyUtils.Numbers/Number.cs
+++ HandyUtils.Numbers/Number.cs
@@ -4,18 +4,6 @@ namespace HandyUtils.Numbers
 {
     public static class Number
     {
-        private static readonly bool[] BytesDivisibleBy4Map = new bool[128]
-        {
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 00..0F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 10..1F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 20..2F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 30..3F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 40..4F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 50..5F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 60..6F
-            true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, // 70..7F
-        };
-
         public static bool IsDivisibleBy2(int number)
         {
             return (number & 1) == 0;
@@ -23,9 +11,7 @@ namespace HandyUtils.Numbers
 
         public static bool IsDivisibleBy4(int number)
         {
-            const byte lowestByteWithBitsSetTo1HigherThanDecimal99 = 0b_0111_1111;
-
-            return BytesDivisibleBy4Map[number & lowestByteWithBitsSetTo1HigherThanDecimal99];
+            return (number & 3) == 0;
         }
     }
 }
